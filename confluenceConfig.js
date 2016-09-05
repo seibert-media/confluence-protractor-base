b95bb547185aef64814944635b7130deb82473b3
Tilman Potthof <tilmanpotthof@googlemail.com>
2016-09-05 18:32:32 +0200

    Allow objects as confluenceConfig

--- confluenceConfig.js
+++ confluenceConfig.js
@@ -2,7 +2,20 @@
 // ignoreSynchronization prevents protractor from waiting for angular
 browser.ignoreSynchronization = true;
 
-var confluenceConfigModule = './config/' + browser.params.confluenceConfig;
-var confluenceConfig = require(confluenceConfigModule);
+var confluenceConfig;
+
+if (typeof browser.params.confluenceConfig === 'string') {
+	var confluenceConfigModule = './config/' + browser.params.confluenceConfig;
+	confluenceConfig = require(confluenceConfigModule);
+}
+else if (typeof browser.params.confluenceConfig === 'object') {
+	confluenceConfig = browser.params.confluenceConfig;
+}
+else {
+	throw new Error('No valid confluenceConfig parameter in protractor config: ', browser.params.confluenceConfig);
+}
+
+console.log('confluenceConfig: ', confluenceConfig);
 
 module.exports = confluenceConfig;
+
