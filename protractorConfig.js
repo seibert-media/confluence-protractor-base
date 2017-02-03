cd013dbed7546a93639b393ca420788a7187b710
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-02-03 17:10:44 +0100

    Update protractor to 5.1.0 and remove workaround for race conditions

--- protractorConfig.js
+++ protractorConfig.js
@@ -39,16 +39,6 @@ exports.config = {
 		var screenshotReporter = require('./jasmineReporters/screenshotReporter');
 		jasmineEnv.addReporter(screenshotReporter);
 
-		/**
-		 * Workaround for race condition
-		 *
-		 * Github Issue: https://github.com/angular/protractor/issues/3777
-		 * Reproduction test: https://github.com/tilmanpotthof/race-condition-in-expected-condition-visibility-of
-		 */
-		var saveExpectedConditions = require('protractor-save-expected-conditions');
-		protractor.ExpectedConditions.visibilityOf = saveExpectedConditions.saveVisibilityOf;
-		protractor.ExpectedConditions.textToBePresentInElement = saveExpectedConditions.saveTextToBePresentInElement;
-
 		// log configured timeouts and reset jasmine timeout
 		var pageObjectUtils = require('./utils/pageObjectUtils');
 		console.log('Initial DEFAULT_ELEMENT_TIMEOUT', pageObjectUtils.DEFAULT_ELEMENT_TIMEOUT);
