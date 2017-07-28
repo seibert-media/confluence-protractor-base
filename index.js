553c335322c66420363f9e433c948a5fe9eec2e5
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-07-28 15:20:38 +0200

    Version v0.1.3

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 module.exports = {
 	confluenceConfig: require('./confluenceConfig'),
 	pageObjects: {
-		ConfluenceLogin: require('./page-objects/ConfluenceLogin')
+		ConfluenceLogin: require('./page-objects/ConfluenceLogin'),
+		ConfluenceBase: require('./page-objects/ConfluenceBase')
 	},
 	utils: {
 		pageObjectUtils: require('./utils/pageObjectUtils')
