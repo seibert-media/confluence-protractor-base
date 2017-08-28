cb6c50c6dbf1e97cb9fae58fafd97d917aaece19
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-08-28 11:26:45 +0200

    Replace get functions with `public readonly`

--- src/page-objects/ConfluenceAction.ts
+++ src/page-objects/ConfluenceAction.ts
@@ -3,28 +3,30 @@ import {OpenPageOptions, pageObjectUtils} from "../utils/pageObjectUtils";
 
 export class ConfluenceAction {
 
-	private readonly _path: string;
+	public readonly path: string;
 
 	constructor(options: { path: string }) {
 		pageObjectUtils.assertNotNull(options.path, 'ConfluenceAction "path" must not be null');
 
-		this._path = options.path;
+		this.path = options.path;
 
 		Object.keys(options).forEach((key) => {
 			this[key] = options[key];
 		});
 	}
 
-	get path(): string {
-		return this._path;
-	}
-
-	public getPath() {
-		return this._path;
+	/**
+	 * Legacy getter for path.
+	 *
+	 * @deprecated
+	 * @returns {string}
+	 */
+	public getPath(): string {
+		return this.path;
 	}
 
 	public open(options?: OpenPageOptions): promise.Promise<any> {
-		return pageObjectUtils.openPage(this._path, options);
+		return pageObjectUtils.openPage(this.path, options);
 	}
 
 }
--- src/page-objects/UserActions.ts
+++ src/page-objects/UserActions.ts
@@ -2,73 +2,36 @@ import {ConfluenceAction} from "./ConfluenceAction";
 
 export class UserActions {
 
-	private readonly _username: string;
-	private readonly _fullname: string;
+	public readonly username: string;
+	public readonly fullname: string;
 
-	private readonly _createUser = new ConfluenceAction({
+	public readonly createUser = new ConfluenceAction({
 		path: "admin/users/createuser.action",
 	});
-	private readonly _removeUser = new ConfluenceAction({
-		path: "admin/users/removeuser.action?_username=" + encodeURIComponent(this._username),
+	public readonly removeUser = new ConfluenceAction({
+		path: "admin/users/removeuser.action?_username=" + encodeURIComponent(this.username),
 	});
-	private readonly _userProfile = new ConfluenceAction({
-		path: "display/~" + encodeURIComponent(this._username),
+	public readonly userProfile = new ConfluenceAction({
+		path: "display/~" + encodeURIComponent(this.username),
 	});
-	private readonly _userAdminView = new ConfluenceAction({
-		path: "admin/users/viewuser.action?_username=" + encodeURIComponent(this._username),
+	public readonly userAdminView = new ConfluenceAction({
+		path: "admin/users/viewuser.action?_username=" + encodeURIComponent(this.username),
 	});
-	private readonly _searchUser = new ConfluenceAction({
-		path: "dosearchsite.action?queryString=" + this._fullname.replace(" ", "+"),
+	public readonly searchUser = new ConfluenceAction({
+		path: "dosearchsite.action?queryString=" + this.fullname.replace(" ", "+"),
 	});
-	private readonly _editUserGroups = new ConfluenceAction({
-		path: "admin/users/editusergroups-start.action?_username=" + encodeURIComponent(this._username),
+	public readonly editUserGroups = new ConfluenceAction({
+		path: "admin/users/editusergroups-start.action?_username=" + encodeURIComponent(this.username),
 	});
-	private readonly _viewPersonalSpace = new ConfluenceAction({
-		path: "spaces/viewspace.action?key=~" + encodeURIComponent(this._username),
+	public readonly viewPersonalSpace = new ConfluenceAction({
+		path: "spaces/viewspace.action?key=~" + encodeURIComponent(this.username),
 	});
-	private readonly _createPersonalSpace = new ConfluenceAction({
+	public readonly createPersonalSpace = new ConfluenceAction({
 		path: "spaces/createpersonalspace.action",
 	});
 
 	constructor(username: string, fullname: string) {
-		this._username = username;
-		this._fullname = fullname;
+		this.username = username;
+		this.fullname = fullname;
 	}
-
-	get username(): string {
-		return this._username;
-	}
-
-	get createUser(): ConfluenceAction {
-		return this._createUser;
-	}
-
-	get removeUser(): ConfluenceAction {
-		return this._removeUser;
-	}
-
-	get userProfile(): ConfluenceAction {
-		return this._userProfile;
-	}
-
-	get userAdminView(): ConfluenceAction {
-		return this._userAdminView;
-	}
-
-	get searchUser(): ConfluenceAction {
-		return this._searchUser;
-	}
-
-	get editUserGroups(): ConfluenceAction {
-		return this._editUserGroups;
-	}
-
-	get viewPersonalSpace(): ConfluenceAction {
-		return this._viewPersonalSpace;
-	}
-
-	get createPersonalSpace(): ConfluenceAction {
-		return this._createPersonalSpace;
-	}
-
 }
