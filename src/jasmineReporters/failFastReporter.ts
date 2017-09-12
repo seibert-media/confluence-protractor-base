ad96aa3093af7a3888c69ca016bb1aa372509567
Tilman Potthof <tilmanpotthof@googlemail.com>
2017-09-12 14:22:03 +0200

    Fix failFastReporter

--- src/jasmineReporters/failFastReporter.ts
+++ src/jasmineReporters/failFastReporter.ts
@@ -1,67 +1,64 @@
-import CustomReporterResult = jasmine.CustomReporterResult;
-
 const _ = require("lodash");
 
-// let enabled = false;
-//
-// let refs;
-//
-// function getSpecReferences() {
-// 	const specs = [];
-// 	const suites = [];
-//
-// 	// Use specFilter to gather references to all specs.
-// 	jasmine.getEnv().specFilter = (spec) => {
-// 		specs.push(spec);
-// 		return true;
-// 	};
-//
-// 	// Wrap jasmine's describe function to gather references to all suites.
-// 	jasmine.getEnv().describe = _.wrap(jasmine.getEnv().describe, (describe) => {
-// 		const args = Array.prototype.slice.call(arguments, 1);
-// 		const suite = describe.apply(null, args);
-// 		suites.push(suite);
-// 		return suite;
-// 	});
-//
-// 	return {
-// 		specs,
-// 		suites,
-// 	};
-// }
-//
-// function disableSpecs() {
-// 	if (!refs) {
-// 		throw new Error("jasmine-fail-fast: Must call init() before calling disableSpecs()!");
-// 	}
-//
-// 	refs.specs.forEach((spec) => {
-// 		spec.disable();
-// 	});
-//
-// 	refs.suites.forEach((suite) => {
-// 		suite.beforeFns = [];
-// 		suite.afterFns = [];
-// 		suite.beforeAllFns = [];
-// 		suite.afterAllFns = [];
-// 	});
-// }
+let enabled = false;
+
+let refs: any;
+
+function getSpecReferences(): any {
+	const specs: any[] = [];
+	const suites: any[] = [];
+
+	// Use specFilter to gather references to all specs.
+	jasmine.getEnv().specFilter = (spec) => {
+		specs.push(spec);
+		return true;
+	};
+
+	// Wrap jasmine's describe function to gather references to all suites.
+	jasmine.getEnv().describe = _.wrap(jasmine.getEnv().describe, (describe: any, ...args: any[]) => {
+		const suite = describe.apply(null, args);
+		suites.push(suite);
+		return suite;
+	});
+
+	return {
+		specs,
+		suites,
+	};
+}
+
+function disableSpecs() {
+	if (!refs) {
+		throw new Error("jasmine-fail-fast: Must call init() before calling disableSpecs()!");
+	}
+
+	refs.specs.forEach((spec: any) => {
+		spec.disable();
+	});
+
+	refs.suites.forEach((suite: any) =>{
+		suite.beforeFns = [];
+		suite.afterFns = [];
+		suite.beforeAllFns = [];
+		suite.afterAllFns = [];
+	});
+}
 
 export const failFastReporter = {
-	init() {
-		// refs = getSpecReferences();
+	init: () => {
+		refs = getSpecReferences();
 	},
-	enable() {
-		// enabled = true;
+	enable: () => {
+		enabled = true;
 	},
-	disable() {
-		// enabled = false;
+	disable: () => {
+		enabled = false;
 	},
-	specDone(result: CustomReporterResult) {
-		// if (result.status !== "failed" || !enabled) {
-		// 	return;
-		// }
-		//
-		// disableSpecs();
+	specDone: (result: any) => {
+		if (result.status !== "failed" || !enabled) {
+			return;
+		}
+
+		disableSpecs();
 	},
 };
