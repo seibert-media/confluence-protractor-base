178a53824a0f25657d742e30032690fed5f9a443
Julian Wolf <jwolf@seibert-media.net>
2017-10-09 13:55:19 +0200

    added EditLock and TimeBomb License as test-resource

---
+++ src/utils/TimeBombLicenses.ts
@@ -0,0 +1,8 @@
+export default class TimeBombLicenses {
+	public static THREE_HOURS_TIMEBOMB = "AAAA9A0ODAoPeNpdj0FPwkAQhe/7KzbxZlLSXTEGkj" +
+			"2AbYBoSxO2SriNdcCNZdrMbqv8e5HqheP7XubLvJusIZkBSzWRSk3VwzQeyyKxUsdKiwR9xa" +
+			"4NriFj0QdZuwrJo9w3LNu6OziS79hj3bTIXuTd8Q15vS/9ORkVi0dG+D1OIKDRSuko1pG+F2" +
+			"kPdXdpTOAOxaDyo9sRVMH1ONAMHAUkoArT79bx6aIp7pZizQcg5wfDLNTgvQMSG+QeeZWY+W" +
+			"Jio235Mo6edrtlNI/Vq3geXj+3JX1S80X/5Mr9R+2pxRyOaGy6sat8IYqOqw/weL3lB7zIa8" +
+			"swLAIULU/BlyZoOVbLPEVGAVAio9Yh3t4CFFBJJd00fLnkkzgexV+T6NsiPVpgX02ck";
+}
